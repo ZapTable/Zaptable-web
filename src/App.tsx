3c339ba510a10b1ad2229aa3af576fa32cf18c9c
Tanmay Nayak <125031648+TanmayN22@users.noreply.github.com>
2025-09-17 22:37:27 +0530

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@
 function App() {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello, Tailwind with Vite!
+    </h1>
+
   )
 }
 
