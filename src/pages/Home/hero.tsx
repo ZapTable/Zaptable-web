f151c6ec3e0bbdb3141bf670ecaeac09f3ae0fe0
Sumitm6879 <sm78810@gmail.com>
2025-09-18 18:22:51 +0530

    changed the hero section font

--- src/pages/Home/hero.tsx
+++ src/pages/Home/hero.tsx
@@ -2,14 +2,14 @@ export const Hero = () => (
   <section className="relative min-h-screen bg-brand- flex flex-col items-center justify-center px-4 font-aspekta overflow-hidden">
   {/* Radial grid background */}
   <div className="absolute inset-0 -z-10 bg-[radial-gradient(#bbbbbb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
-    
+
     {/*Main section*/}
     <div className="max-w-2xl mx-auto text-center">
-      <h1 className="text-5xl font-700 text-brand-dark leading-tight mb-6">
+      <h1 className="text-6xl font-besley font-700 text-brand-dark leading-tight mb-6">
         One tool to <span className="underline decoration-brand-lime decoration-4">manage</span><br />
         your restaurant
       </h1>
-      <p className="font-aspekta font-400 text-brand-gray mb-8">
+      <p className="text-xl font-besley font-400 text-brand-gray mb-8">
         Streamline your restaurant operations with real-time insights and effortless team coordination for seamless, top-notch service every time.
       </p>
       <div className="flex flex-col sm:flex-row items-center gap-4 justify-center mb-10">
@@ -18,4 +18,3 @@ export const Hero = () => (
     </div>
   </section>
 );
-
